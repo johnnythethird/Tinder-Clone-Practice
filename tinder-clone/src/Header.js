b774c289f387bd9422e26e5fcae31c0bcaab77eb
johnnythethird <johnnygc0314@gmail.com>
2022-07-28 22:11:53 -0400

    The First Steps to Tinderland

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+      <h2>
+        I am the Header
+      </h2>
+    </div>
+  )
+}
+
+export default Header
